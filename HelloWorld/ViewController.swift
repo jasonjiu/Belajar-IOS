3dc20f9c9ea92c423c5379ae8fa19c07dbff5945
jasonjiu <43024869+jasonjiu@users.noreply.github.com>
2020-02-20 17:33:38 +0700

    Belajar autolayout
    first commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by PROSIA on 20/02/20.
+//  Copyright © 2020 PROSIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
